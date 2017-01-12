f453653b1d09089f3a720d441bf8d0cc56d5eb31
hamzahaly <hzhaly93@gmail.com>
2017-01-11 16:09:59 -0800

    need to enable searching

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     require_once 'connection.php';
     require_once 'models/player-model.php';
 
-    if(isset($_GET['player'])) {
-        $q = $_GET['title'];
+    if(isset($_GET['llName'])) {
+        $q = $_GET['llName'];
     } else {
         $q = '';
     }
@@ -20,10 +20,48 @@
 <head>
     <meta charset="UTF-8">
     <title>Basketball Players</title>
+
+    <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+  <!-- Icons for Materialize -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+          
 </head>
 <body>
-    <h1>Basketball Players</h1>
-    <form>
-    <input type="search" name="name" placeholder="enter a name" value="<?= htmlentities($q)?>"></form>
+    <div class="container">
+        <h1>Basketball Players</h1>
+        <div class="row">
+            <div class="input-field col s6">
+                <input id="name" type="text" class="validate" value="<?= htmlentities($q)?>">
+                <label class="active" for="first_name2">Name</label>
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <?php foreach ((array) $matches as $match): ?>
+            <tbody>
+                <tr>
+                    <td id="playername"> <?= $name = htmlentities($match['llName']); ?> </td>
+                </tr>
+            </tbody>
+            <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
+    
+
+
+<!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
